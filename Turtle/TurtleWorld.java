d6ef76ba587f7c06e72f90a5683745a97c2f4bca
adriannabanana <afu@NN123-HA-09.HS.sd203.org>
2015-08-20 15:08:18 -0500

    Exploring the turtle life
    Simple methods and such........forward, backwards, right, left, color,
    etc

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -6,8 +6,9 @@ public class TurtleWorld
     {
         World turtleWorld = new World();
         Turtle turtle = new Turtle(turtleWorld);
-        turtle.setPenColor(Color.RED);
+        turtle.setPenColor(Color.CYAN);
         turtle.penDown();
         turtle.forward(50);
+
     }
 }
