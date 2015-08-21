134525e24544b5527aee97bd8328b7950e902f9d
adriannabanana <afu@NN123-HA-09.HS.sd203.org>
2015-08-21 15:08:11 -0500

    Two turtles today!

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -3,12 +3,87 @@ import java.awt.Color;
 public class TurtleWorld
 {
     public static void main(String args[])
+        throws InterruptedException
     {
         World turtleWorld = new World();
         Turtle turtle = new Turtle(turtleWorld);
-        turtle.setPenColor(Color.CYAN);
+        turtle.setPenColor(Color.YELLOW);
         turtle.penDown();
-        turtle.forward(50);
 
+        Turtle friend = new Turtle(turtleWorld);
+        friend.setPenColor(Color.GREEN);
+
+        friend.backward(350);
+        
+        friend.turnRight();
+        friend.penDown();
+        friend.forward(500);
+        friend.backward(500);
+        
+        turtle.forward(5);
+        turtle.turn(5);
+        turtle.forward(5);
+        turtle.turn(5);
+        turtle.forward(5);
+        turtle.turn(5);
+        turtle.forward(5);
+        turtle.turn(5);
+        turtle.forward(5);
+        turtle.turn(5);
+        turtle.forward(5);
+        turtle.turn(5);
+        turtle.forward(5);
+        turtle.turn(5);
+        turtle.forward(5);
+        turtle.turn(5);
+        turtle.forward(5);
+        turtle.turn(5);
+        turtle.forward(5);
+        turtle.turn(5);
+        turtle.forward(5);
+        turtle.turn(5);
+        turtle.forward(5);
+        turtle.turn(5);
+        turtle.forward(5);
+        turtle.turn(5);
+        turtle.forward(5);
+        turtle.turn(5);
+        turtle.forward(5);
+        turtle.turn(5);
+        turtle.forward(5);
+        turtle.turn(5);
+        turtle.forward(5);
+        turtle.turn(5);
+        turtle.forward(5);
+        turtle.turn(5);
+        turtle.forward(5);
+        turtle.turn(5);
+        turtle.forward(5);
+        turtle.turn(5);
+        turtle.forward(5);
+        turtle.turn(5);
+        turtle.forward(5);
+        turtle.turn(5);
+        turtle.forward(5);
+        turtle.turn(5);
+        turtle.forward(5);
+        turtle.turn(5);
+        turtle.forward(5);
+        turtle.turn(5);
+        turtle.forward(5);
+        turtle.turn(5);
+        turtle.forward(5);
+        turtle.turn(5);
+        turtle.forward(5);
+        turtle.turn(5);
+        turtle.forward(5);
+        turtle.turn(5);
+        turtle.forward(5);
+        turtle.turn(5);
+        turtle.forward(5);
+        turtle.turn(5);
+        turtle.forward(5);
+        turtle.turn(5);
+     
     }
 }
